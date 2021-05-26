9f9afaca7f6a6404e4c52948e74a3a0747e849b8
LTrent <lowell.davis@ymail.com>
2021-05-26 18:29:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,13 @@
    output.innerHTML = this.value; /* output value to id=result */
 }
 function compute(){
-  var p,t,r,si,y,v ; /* setting up variables for each input */
+  var p,t,r,si,y, ; /* setting up variables for each input */
   p = document.getElementById("principal").value;
   t = document.getElementById("years").value;
   r = document.getElementById("rate").value;
   si = parseInt((p*t*r)/100 );
   y = new Date().getFullYear()+parseInt(t) /* add current year to selected year */
-   
-    function num() {
-        var v = document.getElementById("principal").value;
-        if (v <= 0) {
-            alert("Value should be a positive number");
-        }
-    }
-
+       
 document.getElementById ('result').innerHTML = "If you deposit "+p ; /* output >> */
 document.getElementById ('result1').innerHTML ="At a rate of: "+r ;  /* output >> */
 document.getElementById ('result2').innerHTML ="you will receive an amount of "+si ; /* output >> */
