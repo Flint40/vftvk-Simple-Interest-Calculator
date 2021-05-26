24828f65c4cd462239e10b172dbfba9c95a3210c
LTrent <lowell.davis@ymail.com>
2021-05-26 17:53:43 -0400

    Update script.js
    Same as noted in css description

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+   var slider = document.getElementById("rate");
+   var output = document.getElementById("rate_val");
+   output.innerHTML = slider.value;
+   slider.oninput = function() { /*control with mouse or keyboars*/
+   output.innerHTML = this.value; /* output value to id=result */
+}
+function compute(){
+  var p,t,r,si,y ; /* setting up variables for each input */
+  p = document.getElementById("principal").value;
+  t = document.getElementById("years").value;
+  r = document.getElementById("rate").value;
+  si = parseInt((p*t*r)/100 );
+  y = new Date().getFullYear()+parseInt(t) /* add current year to selected year */
+
+document.getElementById ('result').innerHTML = "If you deposit "+p ; /* output >> */
+document.getElementById ('result1').innerHTML ="At a rate of: "+r ;  /* output >> */
+document.getElementById ('result2').innerHTML ="you will receive an amount of "+si ; /* output >> */
+document.getElementById ('result3').innerHTML ="in the year of " +y ; /* output >> */
 }
         
