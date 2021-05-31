8581d56c1f9a0b1809f82faa85081e34c2502306
LTrent <lowell.davis@ymail.com>
2021-05-31 17:05:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function compute() {
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
         var interest = principal * years * rate/100;
-        var year = new Date().getFullYear()+parseInt(years); //get presenty year and add to select years
+        var year = new Date().getFullYear()+parseInt(years); //get present year and add to select years
 
         var resultText= "If you deposit \<mark\>" + principal + "\<\/mark\>,\<br\>" +
                         "at an interest rate of %\<mark\>" + rate + "\<\/mark\>.\<br\>" +
