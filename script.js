1a2b9025f24143cfb36c7db02b256dcd04c670be
LTrent <lowell.davis@ymail.com>
2021-05-31 16:57:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-  var slider = document.getElementById("rate");
-   var output = document.getElementById("rate_val");
-   output.innerHTML = slider.value;
-   slider.oninput = function() {
-   output.innerHTML = this.value;
-}
-function compute(){
-  var p,t,r,si,y ;
-  p = document.getElementById("principal").value;
-  t = document.getElementById("years").value;
-  r = document.getElementById("rate").value;
-  si = parseInt((p*t*r)/100 );
-  y = new Date().getFullYear()+parseInt(t)
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0){ //greater then or equal to 0
+        alert("Enter a positive number or else!");
+        document.getElementById("principal").focus(); //bring back to principal input
+        var resultText= "";
+    }else{ 
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate/100;
+        var year = new Date().getFullYear()+parseInt(years); //get presenty year and add to select years
 
-document.getElementById ('result').innerHTML = "If you deposit:" +p ;
-document.getElementById ('result1').innerHTML ="At a rate of: " +r ;
-document.getElementById ('result2').innerHTML ="you will receive "+si ;
-document.getElementById ('result3').innerHTML ="in the year of " +y ;
+        var resultText= "If you deposit \<mark\>" + principal + "\<\/mark\>,\<br\>" +
+                        "at an interest rate of %\<mark\>" + rate + "\<\/mark\>.\<br\>" +
+                        "You will receive an amount of \<mark\>" + interest + "\<\/mark\>,\<br\>" +
+                        "in the year \<mark\>" + year + "\<\/mark\>." // not sure yet why it turns yellow
+    }
+    document.getElementById("result").innerHTML=resultText; // output to result id
+    
+}
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+function validateForm(){
+
 }
         
